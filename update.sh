884830604d289b6ff236fec835813ba6670f3671
Mahmud <mahmudnosirov77@gmail.com>
2023-02-17 20:59:54 +0500

    Decor website

--- update.sh
+++ update.sh
@@ -1,6 +1,9 @@
+echo "Username: mahmudnosirov-abba"
+echo "Password: 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"
+
 cd ../
 sudo rm -r decor
 git clone https://github.com/mahmudnosirov-abba/decor.git
-sleep 10
 cd decor
 git remote set-url origin git@github.com:mahmudnosirov-abba/decor.git
+cd ../
