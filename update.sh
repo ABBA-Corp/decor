27ea6c071ddad649d1e1b6a2dd89a988c0933f24
Mahmud <mahmudnosirov77@gmail.com>
2023-02-17 20:40:18 +0500

    Decor website

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 cd ../
 sudo rm -r decor
 git clone https://github.com/mahmudnosirov-abba/decor.git
-sleep 5
+sleep 10
 cd decor
 git remote set-url origin git@github.com:mahmudnosirov-abba/decor.git
